9dd05888d87e6293a30786827b24a1fb50804f3a
sharp-n <sof8013@gmail.com>
2022-07-18 10:06:52 +0300

    add method for getting constants

--- src/main/java/com/company/SortingMenu.java
+++ src/main/java/com/company/SortingMenu.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public enum SortingMenu {
 
     RETURN_VALUE(0, "Return"),
@@ -7,7 +9,8 @@ public enum SortingMenu {
     TITLE(2, "Title"),
     PAGES(3, "Pages"),
     AUTHOR(4, "Author"),
-    PUBLISHING_DATE(5, "Publishing date");
+    PUBLISHING_DATE(5, "Publishing date"),
+    DEFAULT(-1, "DEFAULT");
 
     private final int num;
     private final String option;
@@ -20,5 +23,13 @@ public enum SortingMenu {
     @Override
     public String toString() {
         return num + " - " + option;
+    }
+
+    public static SortingMenu getByIndex(int index){
+        return Arrays
+                .stream(values())
+                .filter(e -> e.num == index)
+                .findFirst()
+                .orElseGet(() -> DEFAULT);
     }
 }
