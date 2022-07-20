fc8ab6da3fb3985aa02073159cd95f4220fcedf4
shmaliukh <zskat02@gmail.com>
2022-07-20 16:55:36 +0300

    add test for all items in table for printing

--- src/test/java/com/company/TableUtilTest.java
+++ src/test/java/com/company/TableUtilTest.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.convertors.ItemsConvertor;
 import com.company.items.Book;
+import com.company.items.Journal;
 import com.company.items.Newspaper;
 import com.company.table.TableUtil;
 import org.junit.jupiter.api.Assertions;
@@ -44,6 +45,26 @@ class TableUtilTest {
                         new Newspaper(3,"title3", 3),
                         new Newspaper(4,"title4", 4)
                         )
+        ));
+        TableUtil tableUtil = new TableUtil(bookOptions,items, printWriter);
+        tableUtil.printBody();
+
+        assertEquals(expectedString, outputStream.toString());
+    }
+
+    @Test
+    void tesAllTypesOfItemsInTable() {
+        String expectedString ="" +
+                " 1    | title1  | 1           | false             | NULL    | NULL       " + System.lineSeparator() +
+                " 2    | title2  | 2           | false             | NULL    | NULL       " + System.lineSeparator() +
+                " 3    | title3  | some author | 02.11.2022        | 932     | false      " + System.lineSeparator();
+        ItemsConvertor itemsConvertor = new ItemsConvertor();
+        List<List<String>> items = itemsConvertor.itemsToString(new ArrayList<>(
+                Arrays.asList(
+                        new Newspaper(1,"title1", 1),
+                        new Journal(2,"title2", 2),
+                        new Book(3,"title3","some author", new GregorianCalendar(2022,10,2),932)
+                )
         ));
         TableUtil tableUtil = new TableUtil(bookOptions,items, printWriter);
         tableUtil.printBody();
