49036922ac10d935ee6c9657d82a8104a484fc8d
shmaliukh <zskat02@gmail.com>
2022-07-20 12:40:41 +0300

    add newspaper convertor to string list

---
+++ src/main/java/com/company/convertors/NewspaperConvertor.java
@@ -0,0 +1,10 @@
+package com.company.convertors;
+
+import com.company.items.Journal;
+
+public class NewspaperConvertor extends ItemConvertor {
+
+    public NewspaperConvertor(Journal journal) {
+        super(journal);
+    }
+}
