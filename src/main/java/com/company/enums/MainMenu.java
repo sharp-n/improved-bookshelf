f97598f96d33e0f5b69a4aa2bca84d7edcf951fd
sharp-n <sof8013@gmail.com>
2022-07-25 15:35:05 +0300

    restructuring of main menu enum

--- src/main/java/com/company/enums/MainMenu.java
+++ src/main/java/com/company/enums/MainMenu.java
@@ -5,21 +5,9 @@ import java.util.Arrays;
 public enum MainMenu {
 
     EXIT_VALUE(0, "Exit"),
-    ADD_BOOK(1, "Add book"),
-    DELETE_BOOK(2, "Delete book"),
-    TAKE_BOOK(3, "Take book"),
-    RETURN_BOOK(4, "Return book"),
-    SHOW_BOOKS(5, "Show books"),
-    ADD_JOURNAL(6, "Add journal"),
-    DELETE_JOURNAL(7, "Delete journal"),
-    TAKE_JOURNAL(8, "Take journal"),
-    RETURN_JOURNAL(9, "Return journal"),
-    SHOW_JOURNALS(10, "Show journals"),
-    ADD_NEWSPAPER(11, "Add newspaper"),
-    DELETE_NEWSPAPER(12, "Delete newspaper"),
-    TAKE_NEWSPAPER(13, "Take newspaper"),
-    RETURN_NEWSPAPER(14, "Return newspaper"),
-    SHOW_NEWSPAPERS(15, "Show newspapers"),
+    BOOK(1, "Book"),
+    JOURNAL(2, "Journal"),
+    NEWSPAPER(2, "Newspaper"),
     DEFAULT(-1,"Default");
 
     private final int num;
