51f7abf6fdc3711564730f198c8ee0c5935e08b2
sharp-n <sof8013@gmail.com>
2022-07-25 15:52:18 +0300

    create class for files constants

---
+++ src/main/java/com/company/ConstantsForFiles.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ConstantsForFiles {
+
+    public static final  WorkWithFiles WORK_WITH_BOOK_FILE = new WorkWithFiles();
+    public static final WorkWithFiles WORK_WITH_JOURNAL_FILE = new WorkWithFiles();
+    public static final WorkWithFiles WORK_WITH_NEWSPAPER_FILE = new WorkWithFiles();
+
+    public static final WorkWithFiles WORK_WITH_ONE_FILE = new WorkWithFiles();
+
+}
