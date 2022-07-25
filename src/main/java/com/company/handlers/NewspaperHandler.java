8e6e411f167e7f25f4b23b15536fa126321c3256
sharp-n <sof8013@gmail.com>
2022-07-25 11:45:08 +0300

    add realization of createItem method

--- src/main/java/com/company/handlers/NewspaperHandler.java
+++ src/main/java/com/company/handlers/NewspaperHandler.java
@@ -2,6 +2,7 @@ package com.company.handlers;
 
 import com.company.items.Newspaper;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,9 +17,11 @@ public class NewspaperHandler implements ItemHandler<Newspaper>{
     }
 
     @Override
-    public Newspaper createItem(){
-
-        return null;
+    public Newspaper createItem(List<String> options){
+        int itemID = Integer.parseInt(options.get(0));
+        String title = options.get(1);
+        int pages = Integer.parseInt(options.get(4));
+        return new Newspaper(itemID,title,pages);
     }
 
 }
