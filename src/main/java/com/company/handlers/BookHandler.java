d15780c43e1a0f427f48dfbfe89502f4d4485ddf
sharp-n <sof8013@gmail.com>
2022-07-25 11:46:32 +0300

    add realization of createItem method and add validation of publishing date

--- src/main/java/com/company/handlers/BookHandler.java
+++ src/main/java/com/company/handlers/BookHandler.java
@@ -1,9 +1,11 @@
 package com.company.handlers;
 
+import com.company.Librarian;
 import com.company.items.Book;
 import com.company.items.Newspaper;
 
 import java.util.Comparator;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +19,31 @@ public class BookHandler implements ItemHandler<Book>{
     }
 
     @Override
-    public Book createItem(){
+    public Book createItem(List<String> options){
+        int itemID = Integer.parseInt(options.get(0));
+        String title = options.get(1);
+        String author = options.get(2);
+        String [] date = options.get(3).split("\\.");
+        GregorianCalendar publishingDate = new GregorianCalendar(Integer.parseInt(date[0]),Integer.parseInt(date[1]),Integer.parseInt(date[2]));
+        int pages = Integer.parseInt(options.get(4));
+        return new Book(itemID,title, author, publishingDate,pages);
+    }
 
+
+    public GregorianCalendar validateDate(Integer year, Integer month, Integer day) {
+        if (day == null || month == null || year == null) {
+            return null;
+        }
+        if (day > 31) {
+            return null;
+        }
+        if (month > 12) {
+            return null;
+        }
+        if (Librarian.checkItemForValidity(year) && Librarian.checkItemForValidity(month)
+                && Librarian.checkItemForValidity(day)) {
+            return new GregorianCalendar(year, month - 1, day);
+        }
         return null;
     }
 }
