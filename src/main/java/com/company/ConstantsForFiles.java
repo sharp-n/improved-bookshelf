0b1b31205c3ba853aaf95f795ba73977f949842c
sharp-n <sof8013@gmail.com>
2022-07-25 15:59:10 +0300

    make constants non-static

--- src/main/java/com/company/ConstantsForFiles.java
+++ src/main/java/com/company/ConstantsForFiles.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class ConstantsForFiles {
 
-    public static final  WorkWithFiles WORK_WITH_BOOK_FILE = new WorkWithFiles();
-    public static final WorkWithFiles WORK_WITH_JOURNAL_FILE = new WorkWithFiles();
-    public static final WorkWithFiles WORK_WITH_NEWSPAPER_FILE = new WorkWithFiles();
+    public final  WorkWithFiles WORK_WITH_BOOK_FILE = new WorkWithFiles();
+    public final WorkWithFiles WORK_WITH_JOURNAL_FILE = new WorkWithFiles();
+    public final WorkWithFiles WORK_WITH_NEWSPAPER_FILE = new WorkWithFiles();
 
-    public static final WorkWithFiles WORK_WITH_ONE_FILE = new WorkWithFiles();
+    public final WorkWithFiles WORK_WITH_ONE_FILE = new WorkWithFiles();
 
 }
