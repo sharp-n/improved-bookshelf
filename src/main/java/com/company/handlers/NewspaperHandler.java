845ce57652138459855db53d023bd15832135523
sharp-n <sof8013@gmail.com>
2022-07-26 10:48:41 +0300

    add initialization of items menu for newspaper

--- src/main/java/com/company/handlers/NewspaperHandler.java
+++ src/main/java/com/company/handlers/NewspaperHandler.java
@@ -1,12 +1,23 @@
 package com.company.handlers;
 
 import com.company.items.Newspaper;
+import lombok.NoArgsConstructor;
 
+import java.io.PrintWriter;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class NewspaperHandler implements ItemHandler<Newspaper>{
+import static com.company.ConstantsForItemsTable.NEW_LINE;
+import static com.company.enums.ActionsWithNewspapers.*;
+
+@NoArgsConstructor
+public class NewspaperHandler extends ItemHandler<Newspaper>{
+
+    public NewspaperHandler(PrintWriter out, Scanner in) {
+        super(out, in);
+    }
 
     @Override
     public List<Newspaper> getSortedItemsByComparator(List<Newspaper> items, Comparator<Newspaper> comparator) {
@@ -23,4 +34,10 @@ public class NewspaperHandler implements ItemHandler<Newspaper>{
         return new Newspaper(itemID,title,pages);
     }
 
+    @Override
+    public String initItemsMenuText(){
+        return NEW_LINE + ADD_NEWSPAPER + NEW_LINE + DELETE_NEWSPAPER +
+                NEW_LINE + TAKE_NEWSPAPER + NEW_LINE + RETURN_NEWSPAPER +
+                NEW_LINE + SHOW_NEWSPAPER + NEW_LINE;
+    }
 }
