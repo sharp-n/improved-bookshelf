4af462d6560945bbd676af09d624eaa2a60fe9f2
sharp-n <sof8013@gmail.com>
2022-07-28 11:38:19 +0300

    implement abstract methods

--- src/main/java/com/company/work_with_files/WorkWithFilePerType.java
+++ src/main/java/com/company/work_with_files/WorkWithFilePerType.java
@@ -1,8 +1,18 @@
 package com.company.work_with_files;
 
+import java.nio.file.Paths;
+
 public class WorkWithFilePerType extends WorkWithFiles {
 
+    String typeOfClass;
 
+    public WorkWithFilePerType(String userName, String typeOfItem){
+        super(userName);
+        this.typeOfClass = typeOfItem;
+    }
 
+    public void genFilePath(){
+        filePath = Paths.get(pathToDirectoryAsString, ( userName +"_" + typeOfClass.toLowerCase() + ".txt"));
+    }
 
 }
--- src/main/java/com/company/work_with_files/WorkWithOneFile.java
+++ src/main/java/com/company/work_with_files/WorkWithOneFile.java
@@ -1,6 +1,22 @@
 package com.company.work_with_files;
 
+import com.company.items.Item;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class WorkWithOneFile extends WorkWithFiles {
 
+    public WorkWithOneFile(String userName){
+        super(userName);
+    }
 
+    public void genFilePath(){
+        filePath = Paths.get(pathToDirectoryAsString, ( userName +"_items.txt"));
+    }
+
+    @Override
+    public synchronized void addItemToFile(Item itemToAdd) throws IOException {
+        super.addItemToFile(itemToAdd);
+    }
 }
