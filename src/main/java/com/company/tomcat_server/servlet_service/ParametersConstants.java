445b59a22c670a379ca0753d6a141a02baa6d9f7
sharp-n <sof8013@gmail.com>
2022-08-03 15:21:28 +0300

    add constants for parameters type and their values

---
+++ src/main/java/com/company/tomcat_server/servlet_service/ParametersConstants.java
@@ -0,0 +1,15 @@
+package com.company.tomcat_server.servlet_service;
+
+public class ParametersConstants {
+
+    public static final String NAME = "name";
+    public static final String TYPE_OF_WORK_WITH_FILE = "type-of-work";
+    public static final String TYPE_OF_ITEM = "type-of-item";
+
+    public static final String ONE_FILE = "oneFile";
+    public static final String FILE_PER_TYPE = "filePerType";
+    public static final String BOOK = "Book";
+    public static final String JOURNAL = "Journal";
+    public static final String NEWSPAPER = "Newspaper";
+
+}
