b2e3ed7cedc8aeac5024f297385184d2439f6144
sharp-n <sof8013@gmail.com>
2022-08-03 15:21:42 +0300

    add constants for url

---
+++ src/main/java/com/company/tomcat_server/servlet_service/URLConstants.java
@@ -0,0 +1,12 @@
+package com.company.tomcat_server.servlet_service;
+
+public class URLConstants {
+
+    public static final String LOGIN_PAGE = "/login";
+    public static final String FILE_WORK_PAGE = "/file-work-choose";
+
+    public static final String MAIN_MENU_PAGE = "/main";
+    public static final String CHOOSE_ITEM_PAGE = "/choose-item";
+
+    public static final String ADD_BOOK_PAGE = "/add-book";
+}
