052f0ce75023f7d872bc310c4de1b1a4da650f94
sharp-n <sof8013@gmail.com>
2022-08-04 13:05:25 +0300

    add implementation of abstract methods in default handler

--- src/main/java/com/company/handlers/item_handlers/DefaultItemHandler.java
+++ src/main/java/com/company/handlers/item_handlers/DefaultItemHandler.java
@@ -1,20 +1,32 @@
 package com.company.handlers.item_handlers;
 
+import com.company.items.Book;
 import com.company.items.Item;
+import com.company.table.TableUtil;
 import lombok.NoArgsConstructor;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 public class DefaultItemHandler extends ItemHandler<Item> {
 
     @Override
     public List<Item> getSortedItemsByComparator(List<Item> items, Comparator<Item> comparator) {
-        return null;
+        return items.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<List<String>> anyItemsToString(List<Item> items) {
-        return null;
+        List<List<String>> itemsAsStringList = new ArrayList<>();
+        for (Item item: items) {
+            ItemHandler itemHandler = ItemHandlerProvider.getHandlerByClass(item.getClass());
+            itemsAsStringList.add(itemHandler.itemToString(item));
+        }
+        TableUtil tableUtil = new TableUtil(columnTitles,itemsAsStringList, out);
+        tableUtil.validateRows();
+        return tableUtil.getRows();
     }
 }
