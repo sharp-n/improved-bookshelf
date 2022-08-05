b3d41ff57121a9d351fa4a864f2051bc340a3c76
sharp-n <sof8013@gmail.com>
2022-08-05 15:37:05 +0300

    refactor AddBookServlet.java

---
+++ src/main/java/com/company/tomcat_server/AddItemServlet.java
@@ -0,0 +1,102 @@
+package com.company.tomcat_server;
+
+import com.company.User;
+import com.company.enums.FilesMenu;
+import com.company.enums.MainMenu;
+import com.company.handlers.ProjectHandler;
+import com.company.handlers.item_handlers.ItemHandler;
+import com.company.handlers.item_handlers.ItemHandlerProvider;
+import com.company.tomcat_server.servlet_service.*;
+import org.apache.http.client.utils.URIBuilder;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.*;
+
+@WebServlet(
+        name = "AddBookServlet",
+        urlPatterns = {"/add"}
+)
+public class AddItemServlet extends HttpServlet {
+
+    String name = "";
+    String typeOfFileWork = "";
+    String typeOfItem = "";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        System.out.println("AddBookServlet.doGet");
+        ServletOutputStream out = resp.getOutputStream();
+
+        name = req.getParameter(ParametersConstants.NAME);
+        typeOfFileWork = req.getParameter(ParametersConstants.TYPE_OF_WORK_WITH_FILE);
+        typeOfItem = req.getParameter(ParametersConstants.TYPE_OF_ITEM);
+
+        System.out.println(typeOfItem);
+        ServletService servletService = new ServletService();
+        String htmlCode = servletService.getTextFromFile(Paths.get(servletService.pathToHTMLFilesDir.toString(),"add-template.html"));
+
+        String formContent = new HTMLFormBuilder().genForm(ItemHandlerProvider.getHandlerByClass(ItemHandlerProvider.getClassBySimpleNameOfClass(typeOfItem)).genFormContent(),"add");
+
+        htmlCode = htmlCode.replace("{{FORM-CONTENT}}", formContent);
+
+        out.write(htmlCode.getBytes());
+        out.flush();
+
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        ServletService servletService = new ServletService();
+
+
+        ProjectHandler projectHandler = new ProjectHandler(new Scanner(System.in), new PrintWriter(System.out));
+        projectHandler.itemMenuSwitch(MainMenu.getByOption(typeOfItem));
+        projectHandler.fileSwitch(FilesMenu.getByOption(typeOfFileWork), new User(name));
+        ItemHandler itemHandler = projectHandler.getItemHandler();
+
+        List<String> params = itemHandler.convertItemParametersMapToList(req.getParameterMap());
+        params.forEach(o->System.out.println(o));
+
+        String htmlCode = servletService.getTextFromFile(Paths.get(servletService.pathToHTMLFilesDir.toString(),"inform-page-template.html"));
+
+        if (projectHandler.getLibrarian().addItem(itemHandler,params)){
+            htmlCode = htmlCode.replace("{{MESSAGE}}", MessageConstants.SUCCESS_MESSAGE);
+        } else {
+            htmlCode = htmlCode.replace("{{MESSAGE}}", MessageConstants.FAIL_MESSAGE);
+        }
+
+        htmlCode = htmlCode.replace(
+                    "{{URL-ITEMS-MENU}}",
+                    new URIBuilder()
+                        .setPath(URLConstants.CHOOSE_ITEM_PAGE)
+                        .addParameter(ParametersConstants.NAME,name)
+                        .addParameter(ParametersConstants.TYPE_OF_WORK_WITH_FILE,typeOfFileWork)
+                        .toString())
+                .replace(
+                    "{{URL-ACTIONS}}",
+                    new URIBuilder()
+                            .setPath(URLConstants.CHOOSE_ACTION)
+                            .addParameter(ParametersConstants.NAME,name)
+                            .addParameter(ParametersConstants.TYPE_OF_WORK_WITH_FILE,typeOfFileWork)
+                            .addParameter(ParametersConstants.TYPE_OF_ITEM,typeOfItem)
+                            .toString()
+                );
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(htmlCode.getBytes());
+        out.flush();
+
+    }
+
+
+
+}
