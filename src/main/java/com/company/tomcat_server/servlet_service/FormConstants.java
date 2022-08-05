0f701861fe74b6a3a3915d9cb56eec7f4332f036
sharp-n <sof8013@gmail.com>
2022-08-05 15:36:32 +0300

    add constants for html forms and messages

---
+++ src/main/java/com/company/tomcat_server/servlet_service/FormConstants.java
@@ -0,0 +1,11 @@
+package com.company.tomcat_server.servlet_service;
+
+public class FormConstants {
+
+    public static final String TITLE_PARAM = "title";
+    public static final String PAGES_PARAM = "pages";
+    public static final String ITEM_ID_PARAM = "itemID";
+    public static final String AUTHOR_PARAM = "author";
+    public static final String PUBLISHING_DATE_PARAM = "publishingDate";
+    public static final String PUBLISHING_PARAM = "publishing";
+}
---
+++ src/main/java/com/company/tomcat_server/servlet_service/MessageConstants.java
@@ -0,0 +1,8 @@
+package com.company.tomcat_server.servlet_service;
+
+public class MessageConstants {
+
+    public static final String SUCCESS_MESSAGE = "Item is successfully added";
+    public static final String FAIL_MESSAGE = "Oops, something goes wrong... Try again";
+
+}
