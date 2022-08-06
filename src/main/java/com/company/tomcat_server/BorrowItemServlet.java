af98ea90abce80de5e346ab36886fdf44ef98409
sharp-n <sof8013@gmail.com>
2022-08-06 12:06:18 +0300

    implement doGet() method

--- src/main/java/com/company/tomcat_server/BorrowItemServlet.java
+++ src/main/java/com/company/tomcat_server/BorrowItemServlet.java
@@ -1,13 +1,48 @@
 package com.company.tomcat_server;
 
+import com.company.handlers.item_handlers.ItemHandlerProvider;
+import com.company.tomcat_server.servlet_service.ParametersConstants;
+import com.company.tomcat_server.servlet_service.ServletService;
+
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @WebServlet(
         name = "BorrowItemServlet",
         urlPatterns = {"/borrow"}
 )
-public class BorrowItemServlet {
+public class BorrowItemServlet extends HttpServlet {
 
+    String name = "";
+    String typeOfFileWork = "";
+    String typeOfItem = "";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        ServletOutputStream out = resp.getOutputStream();
+
+        name = req.getParameter(ParametersConstants.NAME);
+        typeOfFileWork = req.getParameter(ParametersConstants.TYPE_OF_WORK_WITH_FILE);
+        typeOfItem = req.getParameter(ParametersConstants.TYPE_OF_ITEM);
+
+        ServletService servletService = new ServletService();
+        String htmlCode = servletService.getTextFromFile(Paths.get(servletService.pathToHTMLFilesDir.toString(),"actions-realization-template.html"));
+
+        String formContent = ItemHandlerProvider.getHandlerByClass(ItemHandlerProvider.getClassBySimpleNameOfClass(typeOfItem)).genFormForGettingID("borrow");
+
+        htmlCode = htmlCode.replace("{{FORM-CONTENT}}", formContent);
+
+        out.write(htmlCode.getBytes());
+        out.flush();
+
+        out.close();
+    }
 
 
 }
