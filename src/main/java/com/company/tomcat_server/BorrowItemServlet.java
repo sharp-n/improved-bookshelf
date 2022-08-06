d46e16e1b7b1c0924ad9328f645ebcc61a339b6a
sharp-n <sof8013@gmail.com>
2022-08-06 14:05:12 +0300

    implement doPost() method

--- src/main/java/com/company/tomcat_server/BorrowItemServlet.java
+++ src/main/java/com/company/tomcat_server/BorrowItemServlet.java
@@ -1,6 +1,12 @@
 package com.company.tomcat_server;
 
+import com.company.User;
+import com.company.Validator;
+import com.company.enums.FilesMenu;
+import com.company.enums.MainMenu;
+import com.company.handlers.ProjectHandler;
 import com.company.handlers.item_handlers.ItemHandlerProvider;
+import com.company.tomcat_server.servlet_service.FormConstants;
 import com.company.tomcat_server.servlet_service.ParametersConstants;
 import com.company.tomcat_server.servlet_service.ServletService;
 
@@ -10,7 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 @WebServlet(
         name = "BorrowItemServlet",
@@ -22,16 +30,17 @@ public class BorrowItemServlet extends HttpServlet {
     String typeOfFileWork = "";
     String typeOfItem = "";
 
+    ServletService servletService = new ServletService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        ServletOutputStream out = resp.getOutputStream();
+        ServletOutputStream out = resp.getOutputStream(); // todo optimize usage
 
         name = req.getParameter(ParametersConstants.NAME);
         typeOfFileWork = req.getParameter(ParametersConstants.TYPE_OF_WORK_WITH_FILE);
         typeOfItem = req.getParameter(ParametersConstants.TYPE_OF_ITEM);
 
-        ServletService servletService = new ServletService();
         String htmlCode = servletService.getTextFromFile(Paths.get(servletService.pathToHTMLFilesDir.toString(),"actions-realization-template.html"));
 
         String formContent = ItemHandlerProvider.getHandlerByClass(ItemHandlerProvider.getClassBySimpleNameOfClass(typeOfItem)).genFormForGettingID("borrow");
@@ -44,5 +53,35 @@ public class BorrowItemServlet extends HttpServlet {
         out.close();
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        Integer itemID;
+        try{
+            itemID = Integer.parseInt(req.getParameter(FormConstants.ITEM_ID_PARAM));
+
+        } catch (NumberFormatException nfe){
+            itemID = -1;
+        }
+        itemID = Validator.staticValidateID(itemID);
+
+        String message = "O-ops! Something goes wrong...";
+        if (itemID!=null){
+            // todo implement this case
+            ProjectHandler projectHandler = new ProjectHandler(new Scanner(System.in), new PrintWriter(System.out));
+            projectHandler.itemMenuSwitch(MainMenu.getByOption(typeOfItem));
+            projectHandler.fileSwitch(FilesMenu.getByOption(typeOfFileWork), new User(name));
+            boolean borrowed = projectHandler.getLibrarian().borrowItem(itemID,true,projectHandler.getItemHandler());
+            if (borrowed) {
+                message = "Item is successfully borrowed";
+            }
+        }
+        String htmlCode = servletService.getTextFromFile(Paths.get(servletService.pathToHTMLFilesDir.toString(),"inform-page-template.html"));
+        htmlCode = servletService.replaceURLTemplatesInInformPage(htmlCode,name,typeOfFileWork,typeOfItem).replace("{{MESSAGE}}",message);
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(htmlCode.getBytes());
+        out.flush();
+        out.close();
+    }
 
 }
