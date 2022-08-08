022f7e18d05b01620af1bc0886da0d1d9c05d1fa
sharp-n <sof8013@gmail.com>
2022-08-08 15:20:23 +0300

    add templates constants

---
+++ src/main/java/com/company/tomcat_server/servlet_service/TemplatesConstants.java
@@ -0,0 +1,19 @@
+package com.company.tomcat_server.servlet_service;
+
+public class TemplatesConstants {
+
+    public static final String FORM_TEMPLATE = "{{FORM}}";
+
+    public static final String ERL_TYPE_OF_FILE_WORK_TEMPLATE = "{{URL-TYPE-OF-FILE-WORK}}";
+    public static final String TABLE_TEMPLATE = "{{TABLE}}";
+    public static final String URL_ITEMS_MENU_TEMPLATE = "{{URL-ITEMS-MENU}}";
+    public static final String MESSAGE_TEMPLATE = "{{MESSAGE}}";
+    public static final String URL_ACTIONS_TEMPLATE = "{{URL-ACTIONS}}";
+
+    public static final String URL_SHOW_TEMPLATE = "{{URL-SHOW}}";
+    public static final String URL_ADD_TEMPLATE = "{{URL-ADD}}";
+    public static final String URL_RETURN_TEMPLATE = "{{URL-RETURN}}";
+    public static final String URL_DELETE_TEMPLATE = "{{URL-DELETE}}";
+    public static final String URL_TAKE_TEMPLATE = "{{URL-TAKE}}";
+
+}
