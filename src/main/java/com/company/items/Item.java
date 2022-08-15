a70681e367f71f60788418ea6182bf03b763cba3
sharp-n <sof8013@gmail.com>
2022-08-15 13:59:45 +0300

    add new constructor

--- src/main/java/com/company/items/Item.java
+++ src/main/java/com/company/items/Item.java
@@ -1,10 +1,12 @@
 package com.company.items;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Data
 public class Item {
@@ -13,6 +15,13 @@ public class Item {
     public String title;
     public int pages;
     public boolean borrowed;
+
+    public Item(int itemID, String title, int pages) {
+        this.itemID = itemID;
+        this.title = title;
+        this.pages = pages;
+        this.borrowed = false;
+    }
 
     public void setBorrowed(boolean borrowed) {
         this.borrowed = borrowed;
