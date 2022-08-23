9c338be7f0ed7ddf0a6f4cc42442c94b8e8455db
sharp-n <sof8013@gmail.com>
2022-08-23 16:49:50 +0300

    refactor main method of MainServletsRunner

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 import com.company.handlers.ProjectHandler;
 import com.company.server.Server;
 import com.company.tomcat_server.MainServletsRunner;
+import org.apache.catalina.LifecycleException;
 
+import javax.servlet.ServletException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,10 +14,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws  InterruptedException {
+    public static void main(String[] args) throws  InterruptedException{
 
-        Thread telnetThread = new Thread(()-> Server.main(new String[]{}));
-        Thread serverThread = new Thread(()-> Server.main(new String[]{}));
+        Thread telnetThread = new Thread(()-> Server.main(args));
+        Thread serverThread = new Thread(()-> MainServletsRunner.main(args));
 
         // TODO fix bug on exit from terminal session
 
--- src/main/java/com/company/tomcat_server/MainServletsRunner.java
+++ src/main/java/com/company/tomcat_server/MainServletsRunner.java
@@ -9,17 +9,21 @@ import java.io.File;
 
 public class MainServletsRunner {
 
-    public static void main(String[] args) throws LifecycleException, ServletException { // todo  add work with db
-        String webappDirLocation = "src/main/webapp";
-        Tomcat tomcat = new Tomcat();
+    public static void main(String[] args) {
+        try {
+            String webappDirLocation = "src/main/webapp";
+            Tomcat tomcat = new Tomcat();
 
-        tomcat.setPort(8080);
-        String absolutePath = new File(webappDirLocation).getAbsolutePath();
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("", absolutePath);
-        System.out.println("configuring app with basedir: " + absolutePath);
+            tomcat.setPort(8080);
+            String absolutePath = new File(webappDirLocation).getAbsolutePath();
+            StandardContext ctx = (StandardContext) tomcat.addWebapp("", absolutePath);
+            System.out.println("configuring app with basedir: " + absolutePath);
 
-        tomcat.start();
-        tomcat.getServer().await();
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException | ServletException exception){
+            exception.printStackTrace();
+        }
     }
 
 }
