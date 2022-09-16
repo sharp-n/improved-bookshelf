59126eada0eb8f43b762ac70d9c7843c2094b241
sharp-n <sof8013@gmail.com>
2022-09-16 13:07:32 +0300

    create new classes for constants

---
+++ springboot-app/src/main/java/com/company/springbootapp/constants/ThymeleafVariables.java
@@ -0,0 +1,10 @@
+package com.company.springbootapp.constants;
+
+public class ThymeleafVariables {
+
+    public static final String MESSAGE = "message";
+    public static final String TITLE = "title";
+    public static final String FORM = "form";
+    public static final String REF = "ref_template";
+
+}
