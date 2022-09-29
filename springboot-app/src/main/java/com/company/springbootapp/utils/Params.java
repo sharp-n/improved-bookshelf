cf1db10bee59acc350b8d09daf1e3c91f858d13e
sharp-n <sof8013@gmail.com>
2022-09-29 16:50:44 +0300

    add params class for app

---
+++ springboot-app/src/main/java/com/company/springbootapp/utils/Params.java
@@ -0,0 +1,14 @@
+package com.company.springbootapp.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Params {
+
+    String name;
+    String typeOfWork;
+    String typeOfItem;
+
+}
