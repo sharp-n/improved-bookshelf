d8e35c2703d5e497d84ee579b7d4464c37b7a03b
sharp-n <sof8013@gmail.com>
2022-10-06 11:39:37 +0300

    create repositories and services for entities

---
+++ springboot-app/src/main/java/com/company/db/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.company.db.repositories;
+
+import com.company.db.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+}
