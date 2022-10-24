f8b8d716d7c63cf904518b9cee152a6f44576dd3
sharp-n <sof8013@gmail.com>
2022-10-24 10:35:33 +0300

    add logging to spring boot module

--- springboot-app/src/main/java/com/company/Main.java
+++ springboot-app/src/main/java/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import com.company.controllers.ActionsController;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
