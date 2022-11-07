a0ba8dd0bf493f138ac5554a0287bf0d722753a5
sharp-n <sof8013@gmail.com>
2022-11-07 12:06:15 +0200

    fix some mistakes

--- table-builders/src/main/java/com/company/table/TableConverter.java
+++ table-builders/src/main/java/com/company/table/TableConverter.java
@@ -1,24 +1,34 @@
 package com.company.table;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TableConverter {
 
-    public void fromHtmlToSimpleTable(String htmlTable){
+    private final String RAW_BEGIN = "<tr align=\"center\">";
+    private final String RAW_END = "</tr>";
+    private final String HEADER_CELL = "<th>";
+    private final String HEADER_CELL_END = "</th>";
+    private final String BODY_CELL_END = "</td>";
+    private final String BODY_CELL = "<td>";
 
-        getRaws(htmlTable);
+    public void fromHtmlToSimpleTable(String htmlTable, PrintWriter printWriter){
+        List<String> header = getHeader(htmlTable);
+        List<List<String>> raws = getRaws(htmlTable);
+        TableUtil tableUtil = new TableUtil(header,raws,printWriter);
+        tableUtil.printTable();
     }
 
 
     public List<List<String>> getRaws(String htmlTable){
         List<List<String>> rows = new ArrayList<>();
         String htmlTableBody = replaceRow(htmlTable);
-        while(htmlTableBody.contains("</tr>")){
-            String row = htmlTableBody.substring(htmlTable.indexOf("<tr>")+3,htmlTable.indexOf("</tr>"));
+        while(htmlTableBody.contains(RAW_END)){
+            String row = htmlTableBody.substring(htmlTableBody.indexOf(RAW_BEGIN)+RAW_BEGIN.length(),htmlTableBody.indexOf(RAW_END));
             List<String> rowList = new ArrayList<>();
-            while(row.contains("</td>")){
-                String option = replaceOption(row);
+            while(row.contains(BODY_CELL_END)){
+                String option = row.substring(row.indexOf(BODY_CELL)+BODY_CELL.length(),row.indexOf(BODY_CELL_END));
 
                 rowList.add(option);
                 row = replaceOption(row);
@@ -31,21 +41,24 @@ public class TableConverter {
 
     public List<String> getHeader(String htmlTable){
         List<String> options = new ArrayList<>();
-        String htmlTableHeader = htmlTable.substring(htmlTable.indexOf("<tr>"),htmlTable.indexOf("</tr>"));
-        while(htmlTableHeader.contains("</th>")){
-            String option = htmlTableHeader.substring(htmlTable.indexOf("<th>")+3,htmlTable.indexOf("</th>"));
+        String htmlTableHeader = htmlTable.substring(htmlTable.indexOf(RAW_BEGIN)+RAW_BEGIN.length(),htmlTable.indexOf(RAW_END));
+
+        while(htmlTableHeader.contains(HEADER_CELL_END)){
+            String option = htmlTableHeader.substring(htmlTableHeader.indexOf(HEADER_CELL)+HEADER_CELL.length(),htmlTableHeader.indexOf(HEADER_CELL_END));
+
             options.add(option);
-            htmlTableHeader = htmlTableHeader.replace(htmlTableHeader.substring(htmlTable.indexOf("<th>"),htmlTable.indexOf("</th>")),"");
+            htmlTableHeader = htmlTableHeader.replaceFirst(htmlTableHeader.substring(htmlTableHeader.indexOf(HEADER_CELL),htmlTableHeader.indexOf(HEADER_CELL_END)+HEADER_CELL_END.length()),"");
+
         }
         return options;
     }
 
     public String replaceRow(String table){
-        return table.replace(table.substring(table.indexOf("<tr>"),table.indexOf("</tr>")+4),"");
+        return table.replaceFirst(table.substring(table.indexOf(RAW_BEGIN),table.indexOf(RAW_END) + RAW_END.length()),"");
     }
 
     public String replaceOption(String table){
-        return table.replace(table.substring(table.indexOf("<td>"),table.indexOf("</td>")+4),"");
+        return table.replaceFirst(table.substring(table.indexOf(BODY_CELL),table.indexOf(BODY_CELL_END)+BODY_CELL_END.length()),"");
     }
 
 
