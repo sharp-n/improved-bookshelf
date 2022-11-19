a1b8f1fd5d0937ab8397c86c668dad4bf77875f0
sharp-n <sof8013@gmail.com>
2022-11-19 13:21:52 +0200

    add implementation of MethodsHandler methods

--- methods-for-httpclients/src/main/java/com/company/methods_handlers/feign_client/FeignClientService.java
+++ methods-for-httpclients/src/main/java/com/company/methods_handlers/feign_client/FeignClientService.java
@@ -3,9 +3,15 @@ package com.company.methods_handlers.feign_client;
 import com.company.Item;
 import com.company.ParametersForWeb;
 import com.company.methods_handlers.MethodsHandler;
+import com.company.table.TableConverter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.PrintWriter;
 
 @Service
 public class FeignClientService implements MethodsHandler {
@@ -19,22 +25,34 @@ public class FeignClientService implements MethodsHandler {
 
     @Override
     public void postForAdd(Item item, ParametersForWeb params) {
-        ResponseEntity responseEntity = feignClient.login(params);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ResponseEntity responseEntity = feignClient.add(gson.toJson(item),params.getName(), params.getTypeOfWork(), params.getTypeOfItem());
+        System.out.println(responseEntity.getStatusCode().getReasonPhrase());
     }
 
     @Override
     public void postForDelete(int id, ParametersForWeb params) {
-
+        ResponseEntity responseEntity = feignClient.delete(id, params.getName(), params.getTypeOfWork(), params.getTypeOfItem());
+        System.out.println(responseEntity.getStatusCode().getReasonPhrase());
     }
 
     @Override
     public void postForTakeOrReturn(int id, ParametersForWeb params, boolean toTake) {
-
+        ResponseEntity responseEntity;
+        if(toTake) {
+            responseEntity = feignClient.borrow(id, params.getName(), params.getTypeOfWork(), params.getTypeOfItem());
+        } else {
+            responseEntity = feignClient.returnItem(id, params.getName(), params.getTypeOfWork(), params.getTypeOfItem());
+        }
+        System.out.println(responseEntity.getStatusCode().getReasonPhrase());
     }
 
     @Override
-    public void postForShow(String parameter, ParametersForWeb params) {
-
+    public void postForShow(String comparator, ParametersForWeb params) {
+        String responsePage = feignClient.showItems(params.getName(),params.getTypeOfWork(),params.getTypeOfItem(),comparator);
+        String table = responsePage.substring(responsePage.indexOf("<table"),responsePage.indexOf("</table>"));
+        TableConverter tableConverter = new TableConverter();
+        tableConverter.fromHtmlToSimpleTable(table,new PrintWriter(System.out,true));
     }
 
 }
