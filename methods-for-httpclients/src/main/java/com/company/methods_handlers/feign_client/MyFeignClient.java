ffe6679d575d39faf5c651774f84e0ceb65382c7
sharp-n <sof8013@gmail.com>
2022-11-19 13:09:01 +0200

    add methods to feign client interface

--- methods-for-httpclients/src/main/java/com/company/methods_handlers/feign_client/MyFeignClient.java
+++ methods-for-httpclients/src/main/java/com/company/methods_handlers/feign_client/MyFeignClient.java
@@ -6,14 +6,36 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "feign",url = "http://localhost:8090/")
 public interface MyFeignClient {
 
     @PostMapping("/cookies")
-    public ResponseEntity login(@RequestBody ParametersForWeb params);
+    ResponseEntity login(@RequestBody ParametersForWeb params);
 
+    @PutMapping("/choose-action/add")
+    ResponseEntity add(String item,
+                              @CookieValue("userName") String userName,
+                              @CookieValue("typeOfWork") String typeOfWork,
+                              @CookieValue("typeOfItem") String typeOfItem);
+
+    @DeleteMapping("/choose-action/delete")
+    ResponseEntity delete(int id,
+                                 @CookieValue("userName") String userName,
+                                 @CookieValue("typeOfWork") String typeOfWork,
+                                 @CookieValue("typeOfItem") String typeOfItem);
+
+
+    @PutMapping("/take")
+    ResponseEntity borrow(int id,
+                                 @CookieValue("userName") String userName,
+                                 @CookieValue("typeOfWork") String typeOfWork,
+                                 @CookieValue("typeOfItem") String typeOfItem);
+
+    @PostMapping(value = "/show-items")
+    String showItems(@CookieValue("userName") String userName,
+                            @CookieValue("typeOfWork") String typeOfWork,
+                            @CookieValue("typeOfItem") String typeOfItem,
+                            @RequestParam(name = "sorting-param") String comparator);
 }
